e16233017bd3bf26dd265b8c4cdce9a80b6ee696
DemineyDev <demineydev@gmail.com>
2018-10-24 17:22:25 +0200

    Committing final

--- Problem2_Even_Fibonacci_Numbers/main.js
+++ Problem2_Even_Fibonacci_Numbers/main.js
@@ -1,3 +1,17 @@
 function fiboEvenSum(n) {
+    var first = 1
+    var second = 2
+    var sum = 2
+    var fiboSequence
+    for (var i = 2; i <= n; i++) {
+        fiboSequence = first + second
+        first = second
+        second = fiboSequence
+        if (fiboSequence % 2 == 0) sum += fiboSequence;
+    }
+
     
+    
+
+    return sum
 }
