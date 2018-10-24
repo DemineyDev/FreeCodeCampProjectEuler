bfec6bfc01c42e659ca266682bc2df339a405082
DemineyDev <demineydev@gmail.com>
2018-10-24 16:28:33 +0200

    Committing final answer

--- Problem1_Multiplesof3and5/main.js
+++ Problem1_Multiplesof3and5/main.js
@@ -1,1 +1,27 @@
 
+
+function multiplesOf3and5 (number) {
+    var numberArray = []
+    var sumOfMultiples = 0
+    
+    for (var i = 0; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            numberArray.push(i)
+        }
+    }
+
+    console.log(numberArray)    
+
+    for (var i = 0; i < numberArray.length; i++) {
+        sumOfMultiples += numberArray[i]
+        
+        
+    }
+    return sumOfMultiples
+    
+}
+
+multiplesOf3and5(30)
+
+
+
